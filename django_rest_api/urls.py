ae2ec3e9ca669ee5684d1beca18f479bd8b4d3a1
aidan2129 <aidan2129@gmail.com>
2015-06-26 13:09:23 +0100

    removed api app

--- django_rest_api/urls.py
+++ django_rest_api/urls.py
@@ -11,7 +11,6 @@ admin.autodiscover()
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = patterns('',
-    url(r'^api/', include('api.urls')),
     url(r'^', include('snippets.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
